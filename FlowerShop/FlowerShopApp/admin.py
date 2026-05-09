8858811dd66f0cac48db1ffff6d3a08a81e48791
Simona Nastoska <165785766+nastoskas@users.noreply.github.com>
2026-05-09 22:33:13 +0200

    Update admin.py

--- FlowerShop/FlowerShopApp/admin.py
+++ FlowerShop/FlowerShopApp/admin.py
@@ -5,8 +5,9 @@ class BouquetAdmin(admin.ModelAdmin):
     exclude = ("created_by",)
 
     def save_model(self, request, obj, form, change):
-        obj.user = request.user
-        return super(BouquetAdmin, self).save_model(request, obj, form, change)
+        if not change:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Bouquet, BouquetAdmin)
